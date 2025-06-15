7109055076f1bd1efae44f12a12d6e330f685013
DVnyT <wizzieperry@gmail.com>
2025-06-15 15:48:19 +0530

    first pass - Contract

--- cpp/src/Tensor.cpp
+++ cpp/src/Tensor.cpp
@@ -62,24 +62,32 @@ Tensor::Tensor(const std::vector<uintptr_t>& modes, const std::vector<int64_t>& 
 
 Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutensorHandle_t& globalHandle)
 {
-	cutensorTensorDescriptor_t descA;				// Allocated tensor descriptor
+	/* 
+	 *	Step 1: Describe the tensors in a suitable format 
+	*/
+
+	cutensorTensorDescriptor_t descA;				// Allocated tensor descriptor for A
 	cutensorTensorDescriptor_t descB;
+
+	// TODO: Create a lookup table to initialize indices left in the output Tensor C
+	std::map<uintptr_t, int64_t> lookupC;
+
+	Tensor::Tensor C(&lookupC);					// Output Tensor
 	
 	const uint32_t kAlignment = 128;  				// TODO: Do make this a global variable!
-	
-	const cutensorComputeDescriptor_t descCompute = CUTENSOR_COMPUTE_DESC_32F;	// Precision of contraction
-	
-	cutensorCreateTensorDescriptor_t(globalHandle,			// Initializes cuTENSOR's library context
+		
+	cutensorCreateTensorDescriptor_t(globalHandle,			
 				  &descA,				
 				  A.m_order,
 				  A.m_extents.data(),		
 				  NULL,					// Stride (refer below!)		
-				  CUTENSOR_R_32F,		
+				  CUTENSOR_R_32F,			// Datatype: 32-bit Real Floats
 				  kAlignment);
 	
 	/*
-		A comment about Stride:
-
+	*	A comment about Stride:
+	*
+	*
 	*/
 
 	cutensorCreateTensorDescriptor_t(globalHandle,
@@ -99,15 +107,22 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 				  kAlignment);			
 
 	/*
-		The Operation Descriptor below encodes the contraction, 
-			D_modesD = alpha * opA(A_modesA) * opB(B_modesB) + beta * opC(C_modesC)
-		For our two-tensor contraction, we set beta to 0, and reuse our Tensor Descriptor for C, as the
-		output Tensor D.
+	*	Step 2: Describe the operation to be done on input tensors; create a plan preference, estimate 
+	*	workspace size needed over the course of the operation, then create a proper plan that has information
+	*	about all tunable parameters
+	*	
+	*	The Operation Descriptor below encodes the contraction, 
+	*		D_modesD = alpha * opA(A_modesA) * opB(B_modesB) + beta * opC(C_modesC)
+	*	
+	*	For our two-tensor contraction, we set beta to 0, and reuse our Tensor Descriptor for C, as the
+	*	output Tensor D. Note that modes_D are equivalent to modes_C.
+	*	
+	*	opA() does an operation on all elements of A, we currently use the identity operation.
 	*/
 	
-	
-  	cutensorOperationDescriptor_t descOp;
-	
+  	cutensorOperationDescriptor_t descOp;			// will encode the operation, init. by function below
+	cutensorComputeDescriptor_t descCompute = CUTENSOR_COMPUTE_DESC_32F;	// Precision of contraction
+
 	cutensorCreateContraction(globalHandle,
 			   &descOp,
 			   descA, A.m_modes.data(), CUTENSOR_OP_IDENTITY,	// descA, A.m_modes, opA
@@ -115,19 +130,22 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 			   descC, C.m_modes.data(), CUTENSOR_OP_IDENTITY,
 			   descC, C.m_modes.data(),
 			   descCompute);
-
 	
   	typedef float floatTypeCompute;
   	floatTypeCompute alpha = (floatTypeCompute)1.0f;
   	floatTypeCompute beta = (floatTypeCompute)0.f;
 
-  	const cutensorAlgo_t algo = CUTENSOR_ALGO_DEFAULT;
-	cutensorPlanPreference_t planPref;
+	cutensorPlan_t plan;
+	cutensorPlanPreference_t planPref;					
+	// plan holds the work to be done, in this case a contraction, along with parameters we tune now
+	// planPref narrows the choices of algorithms/variants/kernels to use
   	
-	cutensorCreatePlanPreference(handle, 
+	const cutensorAlgo_t algo = chooseContractionAlgo();			// default is CUTENSOR_ALGO_DEFAULT
+	
+	cutensorCreatePlanPreference(globalHandle, 
 				&planPref, 
 				algo,
-				CUTENSOR_JIT_MODE_NONE));
+				CUTENSOR_JIT_MODE_NONE));			// Not using Just-In-Time compilation
 
 	uint64_t workspaceSizeEstimate{0};
   	const cutensorWorksizePreference_t workspacePref = CUTENSOR_WORKSPACE_DEFAULT;
@@ -137,21 +155,42 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 			       workspacePref,
 			       &workspaceSizeEstimate);
 
-	cutensorPlan_t plan;
 	cutensorCreatePlan(globalHandle,
 		    &plan,
 		    descOp,
 		    planPref,
 		    workspaceSizeEstimate);
+	
+	uint64_t actualWorkspaceSize{0};				// attempt to optimize memory allocated
+	cutensorPlanGetAttribute(globalHandle,
+                plan,
+                CUTENSOR_PLAN_REQUIRED_WORKSPACE,
+                &actualWorkspaceSize,
+                sizeof(actualWorkspaceSize));
 
-	cudaStream_t steam;
+    assert(actualWorkspaceSize <= workspaceSizeEstimate);
+
+    void *work = nullptr;
+    if (actualWorkspaceSize > 0)
+    {
+        cudaMalloc(&work, actualWorkspaceSize);
+        assert(uintptr_t(work) % 128 == 0); // workspace must be aligned to 128 byte-boundary
+    }
+
+	/*
+	 *	Step 3: Actual execution
+	 *
+	 *
+	 * */
+
+	cudaStream_t stream;
 	cudaStreamCreate(&stream);
 
   	double minTimeCUTENSOR = 1e100;
   	
 	for (int i = 0; i < 3; ++i) 
 	{
-    		cudaMemcpy(C_d, C, sizeC, cudaMemcpyHostToDevice);
+    		cudaMemcpy(C.Tensor::getm_pDevice(), C, C.Tensor::getm_byteSize(), cudaMemcpyHostToDevice);
     		cudaDeviceSynchronize();
 
     		GPUTimer timer;
@@ -159,8 +198,8 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 
     		cutensorContract(handle, 
 		       plan, 
-		       (void *)&alpha, A_d, B_d,
-		       (void *)&beta, C_d, C_d, 
+		       (void *)&alpha, A.Tensor::getm_pDevice(), B.Tensor::getm_pDevice(),
+		       (void *)&beta, C.Tensor::getm_pDevice(), C.Tensor::getm_pDevice(), 
 		       work, actualWorkspaceSize, stream);
 
     		auto time = timer.seconds();
@@ -169,7 +208,7 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 
   
 
-  	double transferedBytes = sizeC + sizeA + sizeB;
+  	double transferedBytes = C.get_mbyteSize() + A.get_mbyteSize() + B.get_mbyteSize();
   	transferedBytes += ((float)beta != 0.f) ? sizeC : 0;
   	transferedBytes /= 1e9;
 	
@@ -177,15 +216,9 @@ Tensor::Tensor Contract(const Tensor::Tensor& A, const Tensor::Tensor& B, cutens
 	  gflops / minTimeCUTENSOR,
 	  transferedBytes / minTimeCUTENSOR);
 
-
-
-	cutensorDestroyTensorDescriptor();
-	cutensorDestroyTensorDescriptor();
-	cutensorDestroyTensorDescriptor();
+	// TODO: Free memory!
 	
-	cutensorDestroyOperationDescriptor();
-
-
+	return C;
 }
 
 
